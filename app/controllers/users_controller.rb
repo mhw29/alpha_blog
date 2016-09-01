54253faaeb91738e57a243446b30c63feebcd55d
mahwill29 <mahwill29@gmail.com>
2016-09-01 02:04:37 +0000

    Create show action and add gravatar image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,8 +19,7 @@ class UsersController < ApplicationController
   end
 
   def update
-
-  @user = User.find(params[:id])
+    @user = User.find(params[:id])
 
     if @user.update(user_params)
       flash[:success] = "Your account was updated successfully"
@@ -28,8 +27,14 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
-
   end
+  
+  def show
+    @user = User.find(params[:id])
+    
+    
+  end
+  
 
   private
   def user_params
